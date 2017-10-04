557bb47d63196c911d322486b8bc7527b05f993f
user <tya.hsarme@gmail.com>
2017-10-04 10:52:00 +0300

    رر

--- app/src/main/java/com/hsarme/teya/teyashoppinglist/LogInActivity.java
+++ app/src/main/java/com/hsarme/teya/teyashoppinglist/LogInActivity.java
@@ -2,12 +2,26 @@ package com.hsarme.teya.teyashoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity
+{
+    private EditText etEm;
+    private EditText etP;
+    private Button btin;
+    private Button btup;
+    private Button btforget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        etEm=(EditText)findViewById(R.id.etEm);
+        etP=(EditText)findViewById(R.id.etP);
+        btin=(Button)findViewById(R.id.btin);
+        btup=(Button)findViewById(R.id.btup);
+        btforget=(Button)findViewById(R.id.btforget);
+
     }
 }
