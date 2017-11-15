31030e97589a8e6bd387ca6e0f810ca717803bd9
user <tya.hsarme@gmail.com>
2017-11-15 10:52:47 +0200

    رر

--- app/src/main/java/com/hsarme/teya/teyashoppinglist/AddItemActivity.java
+++ app/src/main/java/com/hsarme/teya/teyashoppinglist/AddItemActivity.java
@@ -2,12 +2,46 @@ package com.hsarme.teya.teyashoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class AddItemActivity extends AppCompatActivity {
+    private EditText etamount;
+    private EditText etname;
+    private EditText etPrice;
+    private EditText etUnits;
+    private ImageButton iBtnImage;
+    private Button btnsave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        etamount=(EditText)findViewById(R.id.etamount);
+        etname=(EditText)findViewById(R.id.etname);
+        etPrice=(EditText)findViewById(R.id.etPrice);
+        etUnits=(EditText)findViewById(R.id.etUnits);
+        iBtnImage=(ImageButton) findViewById(R.id.iBtnImage);
+        btnsave=(Button)findViewById(R.id.btnSave);
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataHandler();
+            }
+        });
     }
-}
+    public void dataHandler(){
+        String stname=etname.getText().toString();
+        String stamount=etamount.getText().toString();
+        String stPrice=etPrice.getText().toString();
+        String stUnits=etUnits.getText().toString();
+        double amount=Double.parseDouble(stamount);
+        double price=Double.parseDouble(stPrice);
+        DatabaseReference;
+        }
+    }
+
